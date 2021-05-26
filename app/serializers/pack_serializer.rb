53a38ab139488ac3ee36f477a1814bef01d481f3
Jabair <jabairk@gmail.com>
2021-05-25 20:30:12 -0400

    MVP working, search/filter not working, author_Name giving problems on backend

--- app/serializers/pack_serializer.rb
+++ app/serializers/pack_serializer.rb
@@ -1,13 +1,13 @@
 class PackSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :genre_id, :name, :description, :image, :preview, :link, :price, :genre_name, :author_name
+  attributes :id, :user_id, :genre_id, :name, :description, :image, :preview, :link, :price, :genre_name
 
   has_many :purchases
   belongs_to :user 
   belongs_to :genre 
 
-  def author_name
-    self.object.user.name 
-  end
+  # def author_name
+  #   self.object.user.name 
+  # end
 
   def genre_name
     self.object.genre.name 
